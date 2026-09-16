8190f492f277d55d93d293c5600fcdee10b7db51
albarbaro <abarbaro@redhat.com>
2026-09-16 14:33:37 +0200

    add debug timeout

--- .ci/pipelines/jobs/ocp-fips-helm.sh
+++ .ci/pipelines/jobs/ocp-fips-helm.sh
@@ -27,6 +27,9 @@ handle_ocp_fips_helm() {
 
   deploy_test_backstage_customization_provider "${NAME_SPACE}"
 
+  log::info "Sleeping for 40 minutes to allow manual cluster debugging..."
+  sleep 2400
+
   run_standard_deployment_tests
 }
 
